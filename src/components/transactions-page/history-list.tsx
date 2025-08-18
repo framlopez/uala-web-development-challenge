2cda6a410fc33412f7e1802fd01d9178c1fc20bf
Francisco Lopez <frammlopez@gmail.com>
2025-08-18 12:10:23 -0300

    Actualizar estilos en globals.css para incluir nuevas variables de color y eliminar el componente HistoryListItemSkeleton, reemplazándolo por un uso directo del componente Skeleton en HistoryList. Mejorar la gestión de errores y la visualización de transacciones en HistoryList, incorporando filtros en la URL.

--- src/components/transactions-page/history-list.tsx
+++ src/components/transactions-page/history-list.tsx
@@ -3,65 +3,87 @@
 import type { TransactionsResponse } from "@/app/api/me/transactions/types";
 import EmptySearchSrc from "@/public/empty-search.png";
 import LIMIT from "@/src/constants/listing-limit";
+import { Skeleton } from "@/src/shadcn/components/ui/skeleton";
 import fetcher from "@/src/utils/fetcher";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
 import HistoryListItem from "./history-list-item";
-import HistoryListItemSkeleton from "./history-list-item-skeleton";
 
 export default function HistoryList() {
-	const { data, isLoading, error } = useSWR<TransactionsResponse>(
-		"/api/me/transactions",
-		fetcher,
-	);
+  const searchParams = useSearchParams();
 
-	if (isLoading) {
-		const skeletons = Array.from({ length: LIMIT });
-		return (
-			<div className="divide-y divide-[#DEE2EC]">
-				{skeletons.map((_, index) => (
-					<HistoryListItemSkeleton key={`skeleton-${index}-${Date.now()}`} />
-				))}
-			</div>
-		);
-	}
+  // Construir la URL con los filtros actuales
+  const queryString = searchParams.toString();
+  const apiUrl = queryString
+    ? `/api/me/transactions?${queryString}`
+    : "/api/me/transactions";
 
-	if (error) {
-		return (
-			<div className="py-4">
-				<Image
-					className="size-[72px] mx-auto"
-					src={EmptySearchSrc}
-					alt="Empty search"
-				/>
-				<p className="text-red-500 mt-4 text-center text-sm">
-					Hubo un error al cargar las transacciones.
-				</p>
-			</div>
-		);
-	}
+  const { data, isLoading, error } = useSWR<TransactionsResponse>(
+    apiUrl,
+    fetcher
+  );
 
-	const emptyTransactions = data?.transactions.length === 0;
-	if (emptyTransactions) {
-		return (
-			<div className="py-4">
-				<Image
-					className="size-[72px] mx-auto"
-					src={EmptySearchSrc}
-					alt="Empty search"
-				/>
-				<p className="text-[#606882] mt-4 text-center text-sm">
-					No hay resultados que mostrar. Podés probar usando los filtros.
-				</p>
-			</div>
-		);
-	}
+  if (isLoading) {
+    const skeletons = Array.from({ length: LIMIT });
+    return (
+      <div className="divide-y divide-[#DEE2EC]">
+        {skeletons.map((_, index) => (
+          <div
+            className="flex items-center gap-2 w-full py-3 px-2 animate-pulse"
+            key={`skeleton-${index}-${Date.now()}`}
+          >
+            <Skeleton className="size-8 rounded-xl flex-shrink-0" />
+            <div className="flex flex-col gap-1 w-full">
+              <Skeleton className="rounded-xl w-40 h-3.5" />
+              <Skeleton className="rounded-xl w-30 h-3.5" />
+            </div>
+            <div className="flex flex-col gap-1 w-30">
+              <Skeleton className="rounded-xl w-full h-3.5" />
+              <Skeleton className="rounded-xl w-full h-3.5" />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-	return (
-		<div className="divide-y divide-[#DEE2EC]">
-			{data?.transactions.map((transaction) => (
-				<HistoryListItem transaction={transaction} key={transaction.id} />
-			))}
-		</div>
-	);
+  if (error) {
+    return (
+      <div className="py-4">
+        <Image
+          className="size-[72px] mx-auto"
+          src={EmptySearchSrc}
+          alt="Empty search"
+        />
+        <p className="text-red-500 mt-4 text-center text-sm">
+          Hubo un error al cargar las transacciones.
+        </p>
+      </div>
+    );
+  }
+
+  const emptyTransactions = data?.transactions.length === 0;
+  if (emptyTransactions) {
+    return (
+      <div className="py-4">
+        <Image
+          className="size-[72px] mx-auto"
+          src={EmptySearchSrc}
+          alt="Empty search"
+        />
+        <p className="text-[#606882] mt-4 text-center text-sm">
+          No hay resultados que mostrar. Podés probar usando los filtros.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="divide-y divide-[#DEE2EC]">
+      {data?.transactions.map((transaction) => (
+        <HistoryListItem transaction={transaction} key={transaction.id} />
+      ))}
+    </div>
+  );
 }
--- src/components/transactions-page/history.tsx
+++ src/components/transactions-page/history.tsx
@@ -18,10 +18,10 @@ export default function History() {
       <div className="flex justify-between items-center">
         <h2>Historial de transacciones</h2>
         <div className="flex items-center">
-          <ButtonIcon className="text-primary" onClick={openFilters}>
+          <ButtonIcon className="text-uala-primary" onClick={openFilters}>
             <FilterIcon className="size-6" />
           </ButtonIcon>
-          <ButtonIcon className="text-primary">
+          <ButtonIcon className="text-uala-primary">
             <DownloadIcon className="size-6" />
           </ButtonIcon>
         </div>
