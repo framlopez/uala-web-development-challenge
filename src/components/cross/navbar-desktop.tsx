08184a237eac609b9bcba240f43756926dd6c1c1
Francisco Lopez <frammlopez@gmail.com>
2025-08-18 11:29:55 -0300

    Refactorizar el componente NavbarDesktop para incluir un manejo de estado de carga y error, utilizando componentes de Avatar y Skeleton para mejorar la presentación de la información del usuario. Se eliminan las importaciones de Image y se optimiza la visualización del avatar.

--- src/components/cross/navbar-desktop.tsx
+++ src/components/cross/navbar-desktop.tsx
@@ -1,32 +1,60 @@
 "use client";
 
 import { GetMeResponse } from "@/app/api/me/types";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/src/shadcn/components/ui/avatar";
+import { Skeleton } from "@/src/shadcn/components/ui/skeleton";
 import fetcher from "@/src/utils/fetcher";
-import Image from "next/image";
 import useSWR from "swr";
 
+function NavbarDesktopWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="hidden lg:block sticky top-0">
+      <div className="bg-white py-5 px-6 flex items-center gap-8 shadow-2xs">
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export default function NavbarDesktop() {
-  const { data, isLoading } = useSWR<GetMeResponse>("/api/me", fetcher);
+  const { data, isLoading, error } = useSWR<GetMeResponse>("/api/me", fetcher);
+
+  if (isLoading) {
+    return (
+      <NavbarDesktopWrapper>
+        <Skeleton className="size-10 rounded-full" />
+        <Skeleton className="size-4 rounded w-20" />
+      </NavbarDesktopWrapper>
+    );
+  }
+
+  if (error || !data) {
+    return (
+      <NavbarDesktopWrapper>
+        <span className="text-xs text-red-500">
+          No se pudo obtener tu información.
+        </span>
+      </NavbarDesktopWrapper>
+    );
+  }
 
   return (
     <div className="hidden lg:block sticky top-0">
       <div className="bg-white py-5 px-6 flex items-center gap-8 shadow-2xs">
-        {data?.avatarUrl ? (
-          <Image
-            className="size-10 rounded-full"
-            src={data.avatarUrl}
-            alt={data.email}
-            width={40}
-            height={40}
-          />
-        ) : isLoading ? (
-          <div className="size-10 bg-background rounded-full animate-pulse" />
-        ) : (
-          <div className="size-10 bg-background rounded-full" />
-        )}
+        <Avatar className="size-10">
+          <AvatarImage src={data.avatarUrl} />
+          <AvatarFallback>
+            {data.firstname.charAt(0)}
+            {data.lastname.charAt(0)}
+          </AvatarFallback>
+        </Avatar>
 
         <span className="font-bold text-base">
-          {data?.firstname} {data?.lastname}
+          {data.firstname} {data.lastname}
         </span>
       </div>
     </div>
